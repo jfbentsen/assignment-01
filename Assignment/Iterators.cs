fd9ad6e8d37b69720ff2b4ff69a197939300b5a9
jfbentsen <jfbentsen@gmail.com>
2022-09-30 10:59:51 +0200

    regex + itterateos

--- Assignment/Iterators.cs
+++ Assignment/Iterators.cs
@@ -1,8 +1,39 @@
-namespace Assignment;
-
-public static class Iterators
+using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Assignment1
 {
-    public static IEnumerable<T> Flatten<T>(IEnumerable<IEnumerable<T>> items) => throw new NotImplementedException();
+    public static class Iterators
+    {
+        public static IEnumerable<T> Flatten<T>(IEnumerable<IEnumerable<T>> items)
+        {
+            foreach (var list in items)
+            {
+                foreach (var item in list)
+                {
+                    yield return item;
+                }
+            }
+        }
 
-    public static IEnumerable<T> Filter<T>(IEnumerable<T> items, Predicate<T> predicate) => throw new NotImplementedException();
+        public static IEnumerable<T> Filter<T>(IEnumerable<T> items, Predicate<T> predicate)
+        {
+            foreach (var item in items)
+            {
+                if(predicate(item))
+                {
+                    yield return item;
+                }
+            }
+        }
+
+        public static bool Even(int i)
+        {
+            if(i%2 == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
 }
--- Assignment/RegExpr.cs
+++ Assignment/RegExpr.cs
@@ -1,10 +1,50 @@
-namespace Assignment;
-
-public static class RegExpr
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+namespace Assignment1
 {
-    public static IEnumerable<string> SplitLine(IEnumerable<string> lines) => throw new NotImplementedException();
+    public static class RegExpr
+    {
+        public static IEnumerable<string> SplitLine(IEnumerable<string> lines)
+        {
+            foreach (var item in lines)
+            {
+                String[] words = item.Split(" ");
+                foreach (var word in words)
+                {
+                    yield return word;
+                };
+            }
+        }
+        
+        public static IEnumerable<(int width, int height)> Resolution(string resolutions)
+        {
+            Match m = Regex.Match(resolutions, "(?<width>[0-9]*)x(?<height>[0-9]*)");
+            var matches = Regex.Matches(resolutions, @"(?<width>[0-9]+)x(?<height>[0-9]+)");
+            string[] splitResolutions = resolutions.Split("x");
+            foreach (Match match in matches)
+            {
+                int height = Int32.Parse(match.Groups["height"].Value);
+                int width = Int32.Parse(match.Groups["width"].Value);
+                yield return (width, height);
+            }   
+        }
 
-    public static IEnumerable<(int width, int height)> Resolution(string resolutions) => throw new NotImplementedException();
+        public static IEnumerable<string> InnerText(string html, string tag)
+        {
+            String regex1 = $@"(?<{tag}><[^b](?:\s|>)[^<>]+>(?<text>[^<]+))";
+            String regex2 = @"(?<partTwo>(?<statTag>[<]{0,4}[>])|(?<endTag>[<\/].{0,4}[>]))";
+        
+            if (Regex.IsMatch(html, regex1)) {
+                foreach (Match m in Regex.Matches(html, regex1)) {
+                    GroupCollection gc = m.Groups;
+                    yield return gc["text"].ToString();
+                }
+            } else {
+                yield return Regex.Replace(html, regex2, "").Trim();
+            }
 
-    public static IEnumerable<string> InnerText(string html, string tag) => throw new NotImplementedException();
+        }
+        
+    }   
 }
